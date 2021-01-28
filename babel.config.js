2de3cffab532547a77da2e36d7f842630ccf739f
Tom Milewski <tmilewski@gmail.com>
2021-01-28 03:32:19 -0500

    Testing updates

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,41 @@
-module.exports = (api) => {
-  api.cache(true)
+// module.exports = (api) => {
+//   api.cache(true)
+
+//   return {
+//     presets: [
+//       // [
+//       //   '@babel/preset-env', 
+//       //   {
+//       //     shippedProposals: true,
+//       //     useBuiltIns: 'usage',
+//       //     corejs: '3',
+//       //     modules: false,
+//       //     targets: 'defaults',
+//       //   }
+//       // ],
+//       // '@babel/preset-react',
+//       // '@babel/preset-typescript',
+//       [
+//         'react-app',
+//         {
+//           flow: false,
+//           typescript: true,
+//           absoluteRuntime: false
+//         }
+//       ]
+//     ],
+//     // plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+//   }
+// }
+
+module.exports = function (api) {
+  api.cache(true);
+  
+  const presets = [ "@babel/preset-env", "@babel/preset-react" ];
+  const plugins = [ "macros" ];
 
   return {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript',
-      ['react-app', { 'flow': false, 'typescript': true, 'absoluteRuntime': false }]
-    ],
-    plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
-  }
+    presets,
+    plugins
+  };
 }
