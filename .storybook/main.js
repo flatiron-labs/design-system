24ea55cc949f5849f6f11f29111005f3eedddec7
Tom Milewski <tmilewski@gmail.com>
2021-02-05 10:33:54 -0500

    Remove extra stories defintion in .storybook/main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,7 @@ module.exports = {
   stories: [
     '../src/docs/Introduction.stories.mdx',
     '../src/docs/*.stories.mdx',
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'
   ],
   addons: [
     '@etchteam/storybook-addon-status',
