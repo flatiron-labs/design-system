245a5b9d1e7b084918fad3b947f29b794d84b3b1
Tom Milewski <tmilewski@gmail.com>
2021-02-22 15:18:16 -0500

    Interim commit

--- rollup.config.js
+++ rollup.config.js
@@ -44,3 +44,67 @@ export default {
     analyze({ summaryOnly: true })
   ]
 }
+
+// import resolve from '@rollup/plugin-node-resolve'
+// import commonjs from '@rollup/plugin-commonjs'
+// import image from '@rollup/plugin-image'
+// import peerDepsExternal from "rollup-plugin-peer-deps-external"
+// import typescript from 'rollup-plugin-typescript2'
+// import { terser } from 'rollup-plugin-terser'
+// import analyze from 'rollup-plugin-analyzer'
+
+// import pkg from './package.json'
+
+// export default {
+//   perf: false,
+//   input: './src/index.ts',
+//   output: [
+//     {
+//       dir: pkg.main,
+//       format: 'cjs',
+//       sourcemap: true
+//     },
+//     {
+//       dir: pkg.module,
+//       format: 'esm',
+//       sourcemap: true
+//     }
+//   ],
+//   plugins: [
+//     peerDepsExternal(),
+//     resolve(),
+//     commonjs(),
+//     typescript({ useTsconfigDeclarationDir: true }),
+//     // typescript({
+//     //   clean: true,
+//     //   rollupCommonJSResolveHack: true,
+//     //   exclude: ['**/*.test.ts', '**/*.stories.ts', '**/*.test.d.ts', '**/*.stories.d.ts']
+//     // }),
+//     image(),
+//     terser(),
+//     analyze({ summaryOnly: true })
+//   ]
+
+//   // external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
+//   // plugins: [
+//   //   autoExternal({
+//   //     builtins: false,
+//   //     dependencies: false,
+//   //     peerDependencies: false
+//   //   }),
+//   //   image(),
+//   //   resolve({
+//   //     jsnext: true,
+//   //     main: true,
+//   //     browser: true
+//   //   }),
+//   //   typescript({
+//   //     clean: true,
+//   //     rollupCommonJSResolveHack: true,
+//   //     exclude: ['**/*.test.ts', '**/*.stories.ts', '**/*.test.d.ts', '**/*.stories.d.ts']
+//   //   }),
+//   //   commonjs(),
+//   //   terser(),
+//   //   analyze({ summaryOnly: true })
+//   // ]
+// }
