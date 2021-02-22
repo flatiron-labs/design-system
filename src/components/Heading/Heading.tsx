584b6d219d92c10c118897932086b1a7ce4bcf3c
ann-schulte-flatironschool <77292899+ann-schulte-flatironschool@users.noreply.github.com>
2021-02-22 16:00:43 -0700

    Add Media Card component (#37)
    * Add Media Card component

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { Font } from '~/styles'
 
 export interface StyledHeadingProps {
+  color?: string
   h1?: boolean
   h2?: boolean
   h3?: boolean
@@ -15,6 +16,7 @@ export interface HeadingProps extends StyledHeadingProps {
 }
 
 const StyledHeading = styled.header<StyledHeadingProps>`
+  color: ${props => props.color ?? props.color};
   font-family: ${Font.firaCode}, monospace;
   font-size: 1.65rem;
   font-weight: ${props => (props.bold ? 'bold' : 'normal')};
