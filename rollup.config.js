032a34d27f66be70e3a49cef9e76cb8e5b5584c6
Tom Milewski <tmilewski@gmail.com>
2021-03-08 14:38:46 -0500

    Enable static assets

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import analyze from 'rollup-plugin-analyzer'
-import copy from 'rollup-plugin-copy'
 
 import pkg from './package.json'
 
@@ -26,9 +25,6 @@ export default {
   ],
   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
   plugins: [
-    copy({
-      targets: [{ src: 'assets/*', dest: 'build' }]
-    }),
     peerDepsExternal(),
     resolve(),
     commonjs(),
